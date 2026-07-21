f1a945ffff14b1e8ea0231457e0770c2887b2c3b
Jayant <saigee2703@gmail.com>
2026-07-21 22:31:47 +0530

    feat: implement playable Grand Prix finale and champion crowning

--- GrandPrix.cpp
+++ GrandPrix.cpp
@@ -1,5 +1,10 @@
 #include "GrandPrix.h"
+#include "Commentary.h"
 #include "Fighter.h"
+#include "Bank.h"
+#include "Timeline.h"
+#include "Popularity.h"
+#include "fightnight.h"
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -106,55 +111,394 @@ std::cout << "\n==============================\n";
               << contender2.Name
               << "\n\n";
 
-    int BaseRating1 =
-        contender1.Strength +
-        contender1.Stamina +
-        contender1.Toughness;
+    int seed1 = -1;
+    int seed2 = -1;
 
-    int BaseRating2 =
-        contender2.Strength +
-        contender2.Stamina +
-        contender2.Toughness;
-
-    int rounds1 = 0;
-    int rounds2 = 0;
-
-    for(int round = 1; round <= 3; round++)
+    for (int i = 0; i < roster.size(); i++)
     {
-        int performance1 =
-            BaseRating1 + (rand() % 61 - 30);
-
-        int performance2 =
-            BaseRating2 + (rand() % 61 - 30);
-
-        if(performance1 > performance2)
+        if(roster[i].Name == contender1.Name)
         {
-            rounds1++;
+            seed1=i;
         }
-        else
+        
+        if(roster[i].Name == contender2.Name)
         {
-            rounds2++;
+            seed2=i;
         }
     }
+    ChampionshipFight(seed1,seed2);
+}
 
-    Fighter champion;
 
-    if(rounds1 > rounds2)
+void ChampionshipFight(int fighter1, int fighter2)
+{
+    std::cout << "\n============================\n";
+    std::cout << "   GRAND PRIX FINAL\n";
+    std::cout << "============================\n\n";
+
+    Fighter & f1 = roster[fighter1];
+    Fighter & f2 = roster[fighter2];
+
+    std::cout<<"\n====================\n\n";
+    ShowDate();
+    std::cout<<"\nBalance: $"<<Money<<"\n";
+    std::cout<<"\n====================\n\n";
+
+    std::cout<<"\n===== FIGHT NIGHT =====\n\n";
+    std::cout<<f1.Name<<" Vs "<<f2.Name<<"\n\n";
+
+    std::cout << f1.Name << "\n";
+    std::cout << "STR: " << f1.Strength << "\n";
+    std::cout << "STA: " << f1.Stamina << "\n";
+    std::cout << "TGH: " << f1.Toughness << "\n\n";
+
+    std::cout << f2.Name << "\n";
+    std::cout << "STR: " << f2.Strength << "\n";
+    std::cout << "STA: " << f2.Stamina << "\n";
+    std::cout << "TGH: " << f2.Toughness << "\n\n";
+
+
+    int BaseRating1 = f1.Strength + f1.Stamina + f1.Toughness;
+    int BaseRating2 = f2.Strength + f2.Stamina + f2.Toughness;
+
+    int RoundsWon1=0;
+    int RoundsWon2=0;
+
+    Fighter* winner = nullptr;
+    bool KOFinish=false;
+
+
+    int betchoice=0;
+    std::cout << "Place your bet:\n";
+    std::cout << "1. " << f1.Name << "\n";
+    std::cout << "2. " << f2.Name << "\n";
+    std::cout<<"\n";
+    std::cout<<"Choice: ";
+    
+    while (true)
+{
+    std::cin >> betchoice;
+
+    if (std::cin.fail())
     {
-        champion = contender1;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        std::cout << "Invalid input!\nChoice: ";
+        continue;
+    }
+
+    if (betchoice != 1 && betchoice != 2)
+    {
+        std::cout << "Choose 1 or 2!\nChoice: ";
+        continue;
+    }
+
+    break;
+}
+
+
+
+    int betamount=0;
+while(true)
+{
+    std::cout << "Bet Amount: $";
+
+    std::cin >> betamount;
+
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        std::cout << "Invalid input!\n";
+        continue;
+    }
+
+    if (betamount > Money)
+    {
+        std::cout << "Insufficient Funds!\n";
+        continue;
+    }
+
+    if (betamount <= 0)
+    {
+        std::cout << "Invalid Bet!\n";
+        continue;
+    }
+
+    break;
+}
+std::cin.ignore(1000,'\n');
+
+
+
+    Fighter* chosenfighter;
+    if(betchoice==1)
+    {
+        chosenfighter = &f1;
+    }
+    else if(betchoice==2)
+    {
+        chosenfighter = &f2;
+    }
+    else 
+    {
+        std::cout << "Invalid Choice!\n";
+        return;
+    }
+
+
+
+
+    for (int round=1; round<=3; round++)
+    {
+        std::cout << "\n===== ROUND "
+              << round
+              << " =====\n\n";
+              commentary();
+              commentary();
+              std::cout<<"\n";
+
+        int form1 = rand() % 61 - 30;
+        int form2 = rand() % 61 - 30;
+
+        int performance1 = BaseRating1 + form1;
+        int performance2 = BaseRating2 + form2;
+
+        int diff = abs(performance1 - performance2);
+
+
+
+        if (performance1 > performance2)
+        {
+            
+
+            if (diff >= 50)
+            {
+                int ko = rand() % 100 + 1;
+                if (ko <= 40)
+                {
+                      std::cout << "\nKNOCKOUT!\n";
+                        KOCommentary(f1, f2);
+                      winner = &f1;
+                      KOFinish=true;
+                      break;
+                    
+                }
+            }
+
+            RoundsWon1++;
+            CloseCommentary(f1,f2,diff);
+            std::cout<<"\n";
+            std::cout
+            << f1.Name
+            << " wins Round "
+            << round
+            << "\n\n";
+
+            
+
+        }
+        else if(performance1 < performance2)
+        {
+            
+            if (diff>=50)
+            {
+                int ko = rand() % 100 + 1;
+                
+                if(ko<=40)
+                {
+                    
+                    std::cout << "\nKNOCKOUT!\n";
+                    KOCommentary(f2, f1);
+                    winner=&f2;
+                    KOFinish=true;
+                    break;
+                }
+            }
+
+            RoundsWon2++;
+            CloseCommentary(f2,f1,diff);
+            std::cout<<"\n";
+            std::cout
+            << f2.Name
+            << " wins Round "
+            << round
+            << "\n\n";
+
+            
+
+
+        }
+
+        else 
+        {
+              if (rand() % 2 == 0)
+    {
+        RoundsWon1++;
+
+        std::cout
+        << "Close round! Judges favor "
+        << f1.Name
+        << "\n";
     }
     else
     {
-        champion = contender2;
+        RoundsWon2++;
+
+        std::cout
+        << "Close round! Judges favor "
+        << f2.Name
+        << "\n";
+    }
+        }
+
+
+        std::cout
+        << "\nSCOREBOARD:\n";
+
+        std::cout
+        << f1.Name
+        << ": "
+        << RoundsWon1
+        << "\n";
+
+        std::cout
+        << f2.Name
+        << ": "
+        << RoundsWon2
+        << "\n";
+
+        if (round < 3)
+        {
+            std::cout
+            << "\n-------------------------\n";
+
+            std::cout
+            << "Press Enter for next round...";
+
+            std::cin.get();
+        }
     }
 
-    std::cout
-        << "Champion: "
-        << champion.Name
-        << "\n\n";
 
-    std::cout
-        << champion.Name
-        << " becomes the inaugural Grand Prix Champion!\n";
+    if (winner == nullptr)
+{
+    if (RoundsWon1 > RoundsWon2)
+    {
+        winner = &f1;
+    }
+    else
+    {
+        winner = &f2;
+    }
+}
 
+if (winner == &f1)
+{
+    f1.Wins++;
+    f2.Losses++;
+
+    f1.SeasonWins++;
+    f2.SeasonLosses++;
+
+    f1.SeasonFights++;
+    f2.SeasonFights++;
+    
+}
+else
+{
+    f2.Wins++;
+    f1.Losses++;
+    
+    f2.SeasonWins++;
+    f1.SeasonLosses++;
+
+    f2.SeasonFights++;
+    f1.SeasonFights++;
+}
+
+bool Upset = false;
+if (winner == &f1)
+{
+    if(BaseRating1 + 40 < BaseRating2)
+    {
+        Upset = true;
+    }
+}
+else
+{
+    if (BaseRating2 + 40 < BaseRating1)
+    {
+        Upset = true;
+    }
+}
+
+
+std::cout
+<< "\n===== FINAL RESULT =====\n\n";
+
+std::cout
+<< "Winner: "
+<< winner->Name
+<< "\n";
+
+   PauseGame();
+
+   std::cout
+<< "\n🏆 GRAND PRIX CHAMPION: "
+<< winner->Name
+<< " 🏆\n\n";
+
+    PauseGame();
+
+    if(winner==&f1)
+    {
+        showHeadline(f1,f2,KOFinish);
+    }
+    else
+    {
+        showHeadline(f2,f1,KOFinish);
+    }
+
+std::cout<<"\n\n";
+
+    std::cout << "===== POST FIGHT DEVELOPMENT =====\n\n";
+if (winner == &f1)
+{  
+    ImproveStats(f1,f2);
+    UpdatePopularity(f1,f2,KOFinish,Upset);
+    
+}
+else
+{
+    ImproveStats(f2,f1);
+    UpdatePopularity(f2,f1,KOFinish,Upset);
+    
+}
+
+
+std::cout << "\n=============================\n";
+
+PauseGame();
+
+
+    if (chosenfighter==winner)
+    {
+        Money=Money+betamount;
+        std::cout << "\nBET WON!\n";
+        std::cout << "Earned $" << betamount << '\n';
+    }
+    else
+    {
+        Money=Money-betamount;
+        std::cout << "\nBET LOST!\n";
+        std::cout << "Lost $" << betamount << '\n';
+    }
+    
+    std::cout << "\nCurrent Balance: $"
+          << Money
+          << "\n\n";
+    std::cout<<"====================\n";
+
+    AdvanceTime();
 }
--- GrandPrix.h
+++ GrandPrix.h
@@ -4,5 +4,6 @@ void ViewStandings();
 bool HasFoughtBefore(int fighter1, int fighter2);
 bool CompleteSeason();
 void RecordMatches(int fighter1, int fighter2);
+void ChampionshipFight(int fighter1, int fighter2);
 void CrownChampion();
 #endif
